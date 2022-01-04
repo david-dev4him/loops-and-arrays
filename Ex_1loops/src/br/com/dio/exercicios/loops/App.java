daaad7feac3ae84985c0c99362f46c4d799aafd7
david-dev4him <davidonshipping@gmail.com>
2022-01-04 00:50:18 -0300

    adiciona condicional

---
+++ Ex_1loops/src/br/com/dio/exercicios/loops/App.java
@@ -0,0 +1,25 @@
+package br.com.dio.exercicios.loops;
+
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+
+      String nome;
+      int idade;
+
+      while(true){
+
+        System.out.println("Nome: ");
+        nome = scan.next();
+        if (nome.equals("0")) break;
+        System.out.println("Idade: ");
+        idade = scan.nextInt();
+      }
+
+      System.out.println("Keep trying here...");
+
+    } 
+    
+ }
